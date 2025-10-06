8de9a055adb10e8c8a11ab0315212892ca567857
ARMOR14 <ambaraarmor653@students.amikom.ac.id>
2025-10-06 09:45:53 +0700

    tugas praktikum 1 30 september 2025 (aboutme_0210)

---
+++ pwl_0210/hello_0210.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program PHP Pertamaku</title>
+</head>
+<body>
+    <?php
+    // Menampilkan kalimat ke dalam browser
+    echo "Halo, ini adalah Program PHP Pertamaku!";
+    ?>
+</body>
+</html>
